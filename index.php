5737bcfa9a26644ae5a61060b057c17ab1c80118
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-28 17:31:01 +0530

    UPdate

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
         public $brand;
         
         public function __construct($color, $brand){
-            $this->color;
-            $this->brand;
+            $this->color = $color;
+            $this->brand = $brand;
         }
         public function __destruct(){
+            // this fuction is call in last of the script
             echo "Car color is {$this->color} and Brand is {$this->brand}";
         }
     }
